33a6b01697278c50953d9468f7514bf4387f879d
zain00abd <zainabflash1998@gmail.com>
2024-09-24 23:41:20 +0300

    add applocation icons

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,27 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
+// استيراد الحزمة next-pwa
+import withPWA from 'next-pwa';
+
+// تطبيق إعدادات PWA
+const pwaConfig = withPWA({
+  pwa: {
+    dest: 'public',  // مجلد الـ public الذي سيتم تخزين ملفات PWA فيه
+    disable: process.env.NODE_ENV === 'development',  // تعطيل في بيئة التطوير
+  }
+});
+
+// إعدادات Next.js الأساسية
+const baseConfig = {
   i18n: {
     locales: ['en'],
     defaultLocale: 'en',
     localeDetection: false,
-  },
+  }
+};
+
+// دمج الإعدادات مع بعضها
+const nextConfig = {
+  ...baseConfig,
+  ...pwaConfig
 };
 
 export default nextConfig;
